281f4e8cf6bc9a81c9f7f2ac54ba7a72950a3474
Jason24 <y.r.ukrainianboy@icloud.com>
2023-03-28 14:34:49 +0200

    CarShop

--- CarShop.py
+++ CarShop.py
@@ -1,2 +1,31 @@
+from os import system
+import time
+from Game import Driver
+from Game import Race
+from Car import Car
+
+car_dict = {
+    "Aston Martin": {"make": "Aston Martin", "model": "DB9", "top_speed": 300, "xp": 9},
+    "Honda Accord": {"make": "Lexus", "model": "Accord", "top_speed": 115, "acceleration": 6},
+    "Ford Mustang": {"make": "Ford", "model": "Mustang", "top_speed": 150, "acceleration": 8},
+    "Chevy Camaro": {"make": "Chevy", "model": "Camaro", "top_speed": 140, "acceleration": 7},
+    "Nissan Maxima": {"make": "Nissan", "model": "Maxima", "top_speed": 125, "acceleration": 6},
+    "Audi A4": {"make": "Audi", "model": "A4", "top_speed": 135, "acceleration": 7},
+    "BMW 3 Series": {"make": "BMW", "model": "3 Series", "top_speed": 140, "acceleration": 8},
+    "Mercedes-Benz C-Class": {"make": "Mercedes-Benz", "model": "C-Class", "top_speed": 145, "acceleration": 9},
+    "Lexus ES": {"make": "Lexus", "model": "ES", "top_speed": 130, "acceleration": 6},
+    "Mazda6": {"make": "Mazda", "model": "6", "top_speed": 120, "acceleration": 5}
+}
+
 class CarShop:
-    pass
+    def __init__(self, car_name, car_model, car_top_speed, car_xp):
+
+
+
+    def car_buy()
+        
+
+    def car_sell()
+        
+    def garage()
+        car_dict
