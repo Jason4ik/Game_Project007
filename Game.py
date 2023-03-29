ff422a5f68fa9b6c1054895480564df3e6e7e7df
SeventhFret <maxxik007@gmail.com>
2023-03-29 10:17:03 +0200

    Changes

--- Game.py
+++ Game.py
@@ -50,6 +50,9 @@ class Game:
     def __init__(self):
         self.drivers = []
 
+    def menu(self):
+        pass
+    
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
             cls._instance = super().__new__(cls, *args, **kwargs)
